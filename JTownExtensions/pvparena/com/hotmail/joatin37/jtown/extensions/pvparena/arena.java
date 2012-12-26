bac8f9a45f4b82e9572bd3521301a77610d6e81d
Joatin <joatin37@hotmail.com>
2012-12-26 12:04:55 +0100

    added extension folder

---
+++ JTownExtensions/pvparena/com/hotmail/joatin37/jtown/extensions/pvparena/arena.java
@@ -0,0 +1,13 @@
+package com.hotmail.joatin37.jtown.extensions.pvparena;
+
+public class arena {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
